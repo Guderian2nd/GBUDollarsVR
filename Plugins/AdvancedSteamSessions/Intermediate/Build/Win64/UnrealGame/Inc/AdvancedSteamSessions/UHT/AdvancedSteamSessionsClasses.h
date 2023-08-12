97c435558ef26a85e04553672a5f80dbdb107ab6
Chae Ho Shin <gshin3@live.com>
2023-08-12 14:02:52 +0900

    Initial Commit

---
+++ Plugins/AdvancedSteamSessions/Intermediate/Build/Win64/UnrealGame/Inc/AdvancedSteamSessions/UHT/AdvancedSteamSessionsClasses.h
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#pragma once
+
+
+#include "../../../../../../../../../Users/user/Documents/Unreal Projects/VRExpPluginExample/Plugins/AdvancedSteamSessions/Source/AdvancedSteamSessions/Classes/AdvancedSteamFriendsLibrary.h"
+#include "../../../../../../../../../Users/user/Documents/Unreal Projects/VRExpPluginExample/Plugins/AdvancedSteamSessions/Source/AdvancedSteamSessions/Classes/AdvancedSteamWorkshopLibrary.h"
+#include "../../../../../../../../../Users/user/Documents/Unreal Projects/VRExpPluginExample/Plugins/AdvancedSteamSessions/Source/AdvancedSteamSessions/Classes/SteamNotificationsSubsystem.h"
+#include "../../../../../../../../../Users/user/Documents/Unreal Projects/VRExpPluginExample/Plugins/AdvancedSteamSessions/Source/AdvancedSteamSessions/Classes/SteamRequestGroupOfficersCallbackProxy.h"
+#include "../../../../../../../../../Users/user/Documents/Unreal Projects/VRExpPluginExample/Plugins/AdvancedSteamSessions/Source/AdvancedSteamSessions/Classes/SteamWSRequestUGCDetailsCallbackProxy.h"
+
---
+++ Plugins/AdvancedSteamSessions/Intermediate/Build/Win64/UnrealGame/Inc/AdvancedSteamSessions/UHT/SteamNotificationsSubsystem.generated.h
@@ -0,0 +1,100 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+// IWYU pragma: private, include "SteamNotificationsSubsystem.h"
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef ADVANCEDSTEAMSESSIONS_SteamNotificationsSubsystem_generated_h
+#error "SteamNotificationsSubsystem.generated.h already included, missing '#pragma once' in SteamNotificationsSubsystem.h"
+#endif
+#define ADVANCEDSTEAMSESSIONS_SteamNotificationsSubsystem_generated_h
+
+#define FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_19_DELEGATE \
+ADVANCEDSTEAMSESSIONS_API void FOnSteamOverlayActivated_DelegateWrapper(const FMulticastScriptDelegate& OnSteamOverlayActivated, bool bOverlayState);
+
+
+#define FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_SPARSE_DATA
+#define FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_RPC_WRAPPERS
+#define FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_RPC_WRAPPERS_NO_PURE_DECLS
+#define FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_ACCESSORS
+#define FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUSteamNotificationsSubsystem(); \
+	friend struct Z_Construct_UClass_USteamNotificationsSubsystem_Statics; \
+public: \
+	DECLARE_CLASS(USteamNotificationsSubsystem, UGameInstanceSubsystem, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/AdvancedSteamSessions"), NO_API) \
+	DECLARE_SERIALIZER(USteamNotificationsSubsystem)
+
+
+#define FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_INCLASS \
+private: \
+	static void StaticRegisterNativesUSteamNotificationsSubsystem(); \
+	friend struct Z_Construct_UClass_USteamNotificationsSubsystem_Statics; \
+public: \
+	DECLARE_CLASS(USteamNotificationsSubsystem, UGameInstanceSubsystem, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/AdvancedSteamSessions"), NO_API) \
+	DECLARE_SERIALIZER(USteamNotificationsSubsystem)
+
+
+#define FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API USteamNotificationsSubsystem(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(USteamNotificationsSubsystem) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USteamNotificationsSubsystem); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USteamNotificationsSubsystem); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USteamNotificationsSubsystem(USteamNotificationsSubsystem&&); \
+	NO_API USteamNotificationsSubsystem(const USteamNotificationsSubsystem&); \
+public: \
+	NO_API virtual ~USteamNotificationsSubsystem();
+
+
+#define FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API USteamNotificationsSubsystem(USteamNotificationsSubsystem&&); \
+	NO_API USteamNotificationsSubsystem(const USteamNotificationsSubsystem&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, USteamNotificationsSubsystem); \
+	DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(USteamNotificationsSubsystem); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(USteamNotificationsSubsystem) \
+	NO_API virtual ~USteamNotificationsSubsystem();
+
+
+#define FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_21_PROLOG
+#define FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_SPARSE_DATA \
+	FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_RPC_WRAPPERS \
+	FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_ACCESSORS \
+	FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_INCLASS \
+	FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_SPARSE_DATA \
+	FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_RPC_WRAPPERS_NO_PURE_DECLS \
+	FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_ACCESSORS \
+	FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_INCLASS_NO_PURE_DECLS \
+	FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h_24_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> ADVANCEDSTEAMSESSIONS_API UClass* StaticClass<class USteamNotificationsSubsystem>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID FID_Users_user_Documents_Unreal_Projects_VRExpPluginExample_Plugins_AdvancedSteamSessions_Source_AdvancedSteamSessions_Classes_SteamNotificationsSubsystem_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
